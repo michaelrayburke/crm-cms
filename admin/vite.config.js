077d4b9530cdecda396ec455bef86e1fbc860972
michaelrayburke <34951495+michaelrayburke@users.noreply.github.com>
2025-10-31 11:40:28 -0700

    Update vite.config.js

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,19 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': 'http://localhost:4000'
-    }
-  }
-});
-
-
-// injected resolve.alias
-import path from 'path';
-export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
