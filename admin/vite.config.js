45c5ceaa1259329d5b06b5c1aa754d563945945b
michaelrayburke <34951495+michaelrayburke@users.noreply.github.com>
2025-11-01 13:14:39 -0700

    Add files via upload

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,16 +1,16 @@
-// admin/vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': '/src',
-      '@utils': '/src/utils'
-    }
+      '@': resolve(__dirname, 'src'),
+      '@utils': resolve(__dirname, 'src/utils'),
+    },
   },
   build: {
-    sourcemap: false
-  }
-})
+    sourcemap: true,
+  },
+});
